d35b0c137ff2cc919b5e4a2f4943919b9b56fb2f
Danny Sasse <danny@travefy.com>
2024-07-15 19:46:27 -0400

    undo all the things. The service can just own it

--- addon/services/cognito.js
+++ addon/services/cognito.js
@@ -6,18 +6,38 @@ import Auth from 'aws-amplify/auth';
 import { set } from '@ember/object';
 import { reject } from 'rsvp';
 
+
+/**
+ * The possible values of nextStep in signUp/signIn flows
+*
+* @param attributes
+*/
+export const CognitoNextSteps = Object.freeze({
+  CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED: "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED", // The user was created with a temporary password and must set a new one. Complete the process with confirmSignIn.
+  CONFIRM_SIGN_IN_WITH_CUSTOM_CHALLENGE: "CONFIRM_SIGN_IN_WITH_CUSTOM_CHALLENGE", // The sign-in must be confirmed with a custom challenge response. Complete the process with confirmSignIn.
+  CONFIRM_SIGN_IN_WITH_TOTP_CODE: "CONFIRM_SIGN_IN_WITH_TOTP_CODE", // The sign-in must be confirmed with a TOTP code from the user. Complete the process with confirmSignIn.
+  CONTINUE_SIGN_IN_WITH_TOTP_SETUP: "CONTINUE_SIGN_IN_WITH_TOTP_SETUP", // The TOTP setup process must be continued. Complete the process with confirmSignIn. -- If this MFA method is required but not set up.
+  CONFIRM_SIGN_IN_WITH_SMS_CODE: "CONFIRM_SIGN_IN_WITH_SMS_CODE", // The sign-in must be confirmed with a SMS code from the user. Complete the process with confirmSignIn.
+  CONTINUE_SIGN_IN_WITH_MFA_SELECTION: "CONTINUE_SIGN_IN_WITH_MFA_SELECTION", // The user must select their mode of MFA verification before signing in. Complete the process with confirmSignIn. -- If this MFA method is required but not set up.
+  RESET_PASSWORD: "RESET_PASSWORD", // The user must reset their password via resetPassword.
+  CONFIRM_SIGN_UP: "CONFIRM_SIGN_UP", // The user hasn't completed the sign-up flow fully and must be confirmed via confirmSignUp.
+  COMPLETE_AUTO_SIGN_IN: "COMPLETE_AUTO_SIGN_IN", // The sign up process needs to complete by invoking the autoSignIn API.
+  DONE: "DONE" // The sign in process has been completed.
+});
+
+const isTesting = false; 
+
 /**
  * @public
  * This is a container for easily accessing the logged-in CognitoUser object,
  * as well as creating others using signUp().
  */
 
-const isTesting = false; 
-
 export default class CognitoService extends Service {
   @service session;
   amplify = Amplify;
   auth = Auth;
+  nextStepOptions = CognitoNextSteps;
 
   willDestroy() {
     super.willDestroy(...arguments);
