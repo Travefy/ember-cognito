ccce02d1705bf4da9d1bb0007eb4d5f07368efbe
Danny Sasse <danny@travefy.com>
2024-07-15 19:35:24 -0400

    well...that didnt work

--- addon/authenticators/cognito.js
+++ addon/authenticators/cognito.js
@@ -2,7 +2,6 @@ import { readOnly } from '@ember/object/computed';
 import { set } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Base from 'ember-simple-auth/authenticators/base';
-import CognitoNextStep from 'ember-cognito/objects/cognito-next-steps';
 
 const currentUserEmailKey = "CognitoAuthenticator.currentUserEmail";
 
--- app/objects/cognito-next-steps.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@travefy/ember-cognito/objects/cognito-next-steps';
